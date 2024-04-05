663b87e12d1dd9912b717c64f5c2e542edd6d639
Samet <sametklou1907@gmail.com>
2024-04-05 10:56:59 +0300

    Initial commit

---
+++ src/test/java/com/pospayment/pospayment/PospaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pospayment.pospayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PospaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
